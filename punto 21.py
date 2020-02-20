38b43e68096793fd7419ee9ee15891fad7404ae8
JuanPabloPerea <36460406+JuanPabloPerea@users.noreply.github.com>
2020-02-20 13:34:26 -0500

    Add files via upload

---
+++ punto 21.py
@@ -0,0 +1,17 @@
+#punto 21
+semana = ["lunes", "martes" , "miercoles" ,"jueves" ,"viernes" ,"sabado" ,"domingo"
+        ]
+dia = input ("ingrese un dia de la semana: ")
+def Sem(dia):
+    for i in semana:
+        if (1 == semana.count(dia)):
+            semana.remove(dia)
+        print(semana)
+Sem(dia)
+
+#punto 29
+
+    
+
+    
+
