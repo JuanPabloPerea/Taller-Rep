760f415db3842484183585172434d025a56303bf
JuanPabloPerea <36460406+JuanPabloPerea@users.noreply.github.com>
2020-02-23 18:05:55 -0500

    Update Punto 35.py

--- Punto 35.py
+++ Punto 35.py
@@ -1,9 +1,7 @@
 x = input ("ingrese un numero: ")
 n = int(x)
-def factorial(n):
-    fac = 1
-    while n > 1 :
-        fac = fac*n
-        n = n-1
-    return fac
-print (factorial(n))
+fac = 1
+while n > 1 :
+    fac = fac*n
+    n = n-1
+print (fac)
