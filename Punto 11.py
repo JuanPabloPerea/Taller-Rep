cc20ceb8c4dd83362d4e5f229fb63172f461672d
JuanPabloPerea <36460406+JuanPabloPerea@users.noreply.github.com>
2020-02-23 18:02:04 -0500

    Update Punto 11.py

--- Punto 11.py
+++ Punto 11.py
@@ -1,7 +1,5 @@
 Valorp = input("ingrese el valor del producto: ")
 Valor = float(Valorp)
-def Iva(Valor):
-    s = Valor * 0.16
-    sol = s+Valor
-    print (sol)
-Iva(Valor)
+s = Valor * 0.16
+sol = s+Valor
+print (sol)
