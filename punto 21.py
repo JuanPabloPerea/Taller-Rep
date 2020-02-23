29bece036e466906515e92c90221eea7a245a851
JuanPabloPerea <36460406+JuanPabloPerea@users.noreply.github.com>
2020-02-23 17:55:04 -0500

    Update punto 21.py

--- punto 21.py
+++ punto 21.py
@@ -2,9 +2,8 @@
 semana = ["lunes", "martes" , "miercoles" ,"jueves" ,"viernes" ,"sabado" ,"domingo"]
 print("el nombre debe ponerse en minusculas")
 dia = input ("ingrese un dia de la semana: ")
-def Sem(dia):
-    for i in semana:
-        if (1 == semana.count(dia)):
-            semana.remove(dia)
-    print(semana)
-Sem(dia)
+for i in semana:
+    if (1 == semana.count(dia)):
+        semana.remove(dia)
+print(semana)
+
