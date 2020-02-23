09ed25ce8fa4518b249cb56a21bacbafb8efca2f
JuanPabloPerea <36460406+JuanPabloPerea@users.noreply.github.com>
2020-02-23 14:32:25 -0500

    Add files via upload

---
+++ Punto 11.py
@@ -0,0 +1,7 @@
+Valorp = input("ingrese el valor del producto: ")
+Valor = float(Valorp)
+def Iva(Valor):
+    s = Valor * 0.16
+    sol = s+Valor
+    print (sol)
+Iva(Valor)
---
+++ Punto 19.py
@@ -0,0 +1,10 @@
+numero = input("ingrese un numero: ")
+x = float(numero)
+def May(x):
+    if (x > 100):
+        print("el numero es mayor que 100")
+    elif (x < 100):
+        print("el numero es menor a 100")
+    elif (x == 100):
+        print("el numero es 100")
+May(x)
