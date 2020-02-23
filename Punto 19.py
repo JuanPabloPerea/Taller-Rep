ea0749492448ed9098ab9dfd196eaabd625b922b
JuanPabloPerea <36460406+JuanPabloPerea@users.noreply.github.com>
2020-02-23 18:02:48 -0500

    Update Punto 19.py

--- Punto 19.py
+++ Punto 19.py
@@ -1,10 +1,8 @@
 numero = input("ingrese un numero: ")
 x = float(numero)
-def May(x):
-    if (x > 100):
-        print("el numero es mayor que 100")
-    elif (x < 100):
-        print("el numero es menor a 100")
-    elif (x == 100):
-        print("el numero es 100")
-May(x)
+if (x > 100):
+    print("el numero es mayor que 100")
+elif (x < 100):
+    print("el numero es menor a 100")
+elif (x == 100):
+    print("el numero es 100")
