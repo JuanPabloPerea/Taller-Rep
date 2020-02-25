02e990973b59c4196326e25a52fb432c7a20bdb8
JuanPabloPerea <36460406+JuanPabloPerea@users.noreply.github.com>
2020-02-25 09:35:20 -0500

    Add files via upload

---
+++ Punto 56.py
@@ -0,0 +1,5 @@
+cadena = input("ingrese una Frase: ")
+list = [cadena]
+x=cadena.replace(" ","")
+print (cadena)
+print (x)
---
+++ Punto 62.py
@@ -0,0 +1,7 @@
+cadena1 = input ("ingrese una frase: ")
+cadena2 = input ("ingrese otra frase: ")
+if (cadena1==cadena2):
+    print ("las frases ingresadas son identicas")
+else:
+    print ("las frases ingresados no son iguales")
+
